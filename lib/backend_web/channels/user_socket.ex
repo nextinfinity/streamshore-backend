c6b57ba7ea76af3b84f8590647e1afd5c0e5191e
Next Infinity <xgix57@gmail.com>
2020-02-16 22:11:24 -0500

    Create room chat channel

--- lib/backend_web/channels/user_socket.ex
+++ lib/backend_web/channels/user_socket.ex
@@ -2,7 +2,7 @@ defmodule BackendWeb.UserSocket do
   use Phoenix.Socket
 
   ## Channels
-  # channel "room:*", BackendWeb.RoomChannel
+  channel "room_chat:*", BackendWeb.RoomChatChannel
 
   # Socket params are passed from the client and can
   # be used to verify and authenticate a user. After
