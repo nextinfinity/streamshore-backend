ec5bad4730c3478f78cb9436411ecdfd25c79a86
Next Infinity <xgix57@gmail.com>
2020-02-22 14:47:40 -0500

    Fix CORS headers

--- mix.exs
+++ mix.exs
@@ -43,7 +43,9 @@ defmodule Streamshore.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:dictionary, github: "rawandrew/dictionary"}
+      {:dictionary, github: "rawandrew/dictionary"},
+      {:plug, "~> 1.0"},
+      {:corsica, "~> 1.0"}
     ]
   end
 
