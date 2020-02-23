8b37a3bd88b353a5386c2f13d980c983d0e6e1a1
Next Infinity <xgix57@gmail.com>
2020-02-23 17:31:57 -0500

    Video fetching, converting, and serving

--- lib/streamshore_web/controllers/room_controller.ex
+++ lib/streamshore_web/controllers/room_controller.ex
@@ -14,6 +14,9 @@ defmodule StreamshoreWeb.RoomController do
   end
 
   def show(conn, _params) do
+    System.cmd("E:/Downloads/youtube-dl.exe", ["https://www.youtube.com/watch?v=rezzjJ4NtK0", "-o", "video.mp4"])
+    Task.async(fn -> System.cmd("E:/Downloads/test/ffmpeg.exe", ["-i", System.cwd() <> "/video.mp4", "-f", "hls", System.cwd() <> "/video/index.m3u8"]) end)
+    json(conn, %{video: System.cwd() <> "/video/index.m3u8"})
     # TODO: show room info
   end
 
