02bc406f1752a738500fcc25bd4eac4bf4c8481d
jsovich <42979012+jsovich@users.noreply.github.com>
2020-02-23 20:15:41 -0500

    Started User Info Validation
    Start of user info validation

--- lib/streamshore/accounts/user.ex
+++ lib/streamshore/accounts/user.ex
@@ -14,19 +14,37 @@ defmodule Streamshore.User do
     def register_changeset(user, params \\ %{}) do
     user
     |> cast(params, [:username, :email, :password])
+    |> valid_username()
     |> valid_password()
     |> valid_email()
     end
 
-    def valid_password(changeset) do
-        password = get_field(changeset, :password)
-        #TODO: Validate
-        changeset
+    def valid_username(changeset) do
+        # TODO: Check for unique username
     end
 
+    def valid_password(changeset) do
+        password = get_field(changeset, :password)
+        # TODO: Hash password
+        left_map = %{key: password}
+        if (match?(left_map, "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})")) do
+            changeset
+        else 
+            add_error(changeset, :password, "is not a valid password")
+        end
+    end
+
+    # Checks to see if the email that has been set to the changeset is a valid email.
+    # If it is not, then it errors, if it is, then it continues.
     def valid_email(changeset) do
         email = get_field(changeset, :email)
-        #TODO: Validate
-        changeset
+        #TODO: Add check for unique email
+        left_map = %{key: email}
+        #TODO: Add Regex for email
+        if (match?(left_map, "")) do
+            changeset
+        else 
+            add_error(changeset, :email, "is not a valid email")
+        end
     end
 end
