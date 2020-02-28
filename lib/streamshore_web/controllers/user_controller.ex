e0a53d65ab5d2d07601678d03d63b3b6ef605695
jsovich <jsovich@purdue.edu>
2020-02-28 09:19:04 -0500

    Error Handling
    Now returns true or false if insert into repo is successful or fails, still need to return the username to the front end

--- lib/streamshore_web/controllers/user_controller.ex
+++ lib/streamshore_web/controllers/user_controller.ex
@@ -2,24 +2,24 @@ defmodule StreamshoreWeb.UserController do
   use StreamshoreWeb, :controller
   alias Streamshore.Repo
   alias Streamshore.User
-
+  
   def index(conn, _params) do
     users = Repo.all(User)
     render(conn, "index.html", users: users)
   end
 
   def create(conn, params) do
-    successful = params
+    successful =
     %Streamshore.User{}
     |> User.changeset(params)
     |> Repo.insert()
 
     case successful do
-      {:ok, %Streamshore.User{}} ->
-        json(conn, %{success: true}) # This is whatever message the frontend wants
+      {:ok, schema}->
+        json(conn, %{success: true}) # Need to return the username
 
-      {:error, changeset} ->
-        json(conn, %{success: false}) # Whatever failure message the frontend wants
+      {:error, changeset}->
+        json(conn, %{success: false})
     end
   end
 
