d58d2a917e8e56a2352b94fa48e29929f31f3548
nextinfinity <xgix57@gmail.com>
2020-02-28 10:49:10 -0500

    Video syncing

--- lib/streamshore/queue_manager.ex
+++ lib/streamshore/queue_manager.ex
@@ -29,6 +29,7 @@ defmodule Streamshore.QueueManager do
     IO.inspect(room_data)
     Videos.set(room, room_data)
     StreamshoreWeb.Endpoint.broadcast("room:" <> room, "queue", %{videos: room_data[:queue]})
+    play_next(room)
   end
 
   def remove_from_queue(room, index) do
@@ -38,12 +39,15 @@ defmodule Streamshore.QueueManager do
   def play_next(room) do
     room_data = Videos.get(room)
     Map.put(room_data, :playing, nil)
-    if (length(room_data[:queue]) > 0) do
+    room_data = if (length(room_data[:queue]) > 0) do
+      room_data = Videos.get(room)
       {next_video, queue} = List.pop_at(room_data[:queue], 0)
-      Map.put(room_data, :playing, next_video)
-      Map.put(room_data, :queue, queue)
+      next_video = Map.put(next_video, :start, get_seconds())
+      room_data = Map.put(room_data, :playing, next_video)
+      room_data = Map.put(room_data, :queue, queue)
       Videos.set(room, room_data)
-      StreamshoreWeb.Endpoint.broadcast("room:" <> room, "queue", room_data[:queue])
+      StreamshoreWeb.Endpoint.broadcast("room:" <> room, "queue", %{videos: room_data[:queue]})
+      room_data
     end
     StreamshoreWeb.Endpoint.broadcast("room:" <> room, "video", %{video: room_data[:playing]})
   end
@@ -54,11 +58,11 @@ defmodule Streamshore.QueueManager do
     Enum.each(Videos.keys, fn room ->
       if Videos.get(room)[:playing] do
         runtime = current_time - Videos.get(room)[:playing][:start]
-        if runtime >= Videos.get(room)[:playing][:length] do
-          play_next(room)
-        else
+        #if runtime >= Videos.get(room)[:playing][:length] do
+        #  play_next(room)
+        #else
           StreamshoreWeb.Endpoint.broadcast("room:room", "time", %{time: runtime})
-        end
+        #end
       end
     end)
   end
