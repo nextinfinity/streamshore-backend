c483dd939a8846579419de3fcf4de0a72de29fce
Next Infinity <xgix57@gmail.com>
2020-03-06 18:04:24 -0500

    http, deploy

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,7 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :streamshore, StreamshoreWeb.Endpoint,
-  url: [port: 80],
+  http: [port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
