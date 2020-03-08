11878808c56c8a74f0a94099d21f78375318482b
Nancy Agarwal <nancyagarwal@Nancys-MacBook-Pro.local>
2020-03-07 21:57:44 -0500

    Login

--- lib/streamshore_web/controllers/session_controller.ex
+++ lib/streamshore_web/controllers/session_controller.ex
@@ -1,11 +1,13 @@
 defmodule StreamshoreWeb.SessionController do
   use StreamshoreWeb, :controller
+  alias Streamshore.Repo
   import Dictionary
 
-  def show(conn, _params) do
+  def show(_conn, _params) do
     # TODO: show session info
   end
 
+  @spec create(Plug.Conn.t(), any) :: Plug.Conn.t()
   def create(conn, params) do
     if (Enum.count(params) != 0) do
       # # TODO: create session (login)
@@ -27,8 +29,35 @@ defmodule StreamshoreWeb.SessionController do
     json(conn, %{name: username})
   end
 
-  def delete(conn, _params) do
+  def process_login(conn, params) do
+    if is_nil(do_login(params["email"], params["password"])) do
+      conn
+      |> put_flash(:redir, params["redir"])
+      |> put_flash(:error, "Login failed") |> redirect(to: "/users/login") |> halt
+    else
+      conn = put_session(conn, :user, params["email"])
+      conn
+        |> put_flash(:info, "Thanks for logging in!")
+        |> redirect(to: params["redir"] || "/")
+    end
+  end
+
+  def do_login(email, password) do
+    user = Repo.get_by(user, [email: email])
+    if user do
+      if Comeonin.Bcrypt.checkpw(password, user["password"]) do
+        user
+      else
+        nil
+      end
+    else
+      nil
+    end
+  end
+
+  def delete(_conn, _params) do
     # TODO: delete session (logout)
   end
 
 end
+
