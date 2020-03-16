91dd2af110623df7138b14ff64441ca5fbdafa2c
Next Infinity <xgix57@gmail.com>
2020-03-16 16:40:26 -0400

    Permission Routes

---
+++ lib/streamshore_web/controllers/permission_controller.ex
@@ -0,0 +1,16 @@
+defmodule StreamshoreWeb.PermissionController do
+  use StreamshoreWeb, :controller
+
+  def index(conn, _params) do
+    # TODO: list
+  end
+
+  def show(conn, _params) do
+    # TODO: show permission info
+  end
+
+  def update(conn, _params) do
+    # TODO: edit permission
+  end
+
+end
