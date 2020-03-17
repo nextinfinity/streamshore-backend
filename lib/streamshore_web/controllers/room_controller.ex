4f4a3eae41f0a0a35fd1a5fef7110e7eae5daa46
Next Infinity <xgix57@gmail.com>
2020-03-16 20:13:10 -0400

    Check if room exists

--- lib/streamshore_web/controllers/room_controller.ex
+++ lib/streamshore_web/controllers/room_controller.ex
@@ -7,9 +7,14 @@ defmodule StreamshoreWeb.RoomController do
     # TODO: list
   end
 
-  def show(conn, _params) do
-    json(conn, %{video: "https://d2zihajmogu5jn.cloudfront.net/bipbop-advanced/bipbop_16x9_variant.m3u8"})
-    # TODO: show room info
+  def show(conn, params) do
+    room = Repo.get_by(Room, roomName: params["id"])
+    success = if room do
+      true
+    else
+      false
+    end
+    json(conn, %{success: success})
   end
 
   def create(conn, params) do
