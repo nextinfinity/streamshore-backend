f489530748ed14c42191b0023f600927db5ae200
jsovich <jsovich@purdue.edu>
2020-03-22 19:07:48 -0400

    Friend List Creation Testing

---
+++ test/streamshore_web/controllers/friends_controller_test.exs
@@ -0,0 +1,30 @@
+defmodule FriendsControllerTest do
+    use StreamshoreWeb.ConnCase
+
+    test "Creating friend connection", %{conn: conn} do
+        friender = "Tester1"
+        friendee = "Tester2"
+        # insert users into database
+        conn = post(conn, Routes.user_path(conn, :create), %{email: "Test@Test.com", username: friender, password: "$Test123"})
+        assert json_response(conn, 200) == %{"success" => true, "username" => "Tester1"}
+        conn = post(conn, Routes.user_path(conn, :create), %{email: "Test@Tester.com", username: friendee, password: "$Test123"})
+        assert json_response(conn, 200) == %{"success" => true, "username" => "Tester2"}
+        # send friend request
+        conn = post(conn, Routes.user_friend_path(conn, :create, friender), %{friendee: friendee})
+        assert json_response(conn, 200) == %{"success" => true}
+        #accept friend request
+        conn = put(conn, Routes.user_friend_path(conn, :update, friendee, friender), %{accepted: 1})
+        assert json_response(conn, 200) == %{"success" => true}
+    end
+
+    test "No existing user", %{conn: conn} do
+        friender = "Tester1"
+        friendee = "Tester2"
+        # insert users into database
+        conn = post(conn, Routes.user_path(conn, :create), %{email: "Test@Test.com", username: friender, password: "$Test123"})
+        assert json_response(conn, 200) == %{"success" => true, "username" => "Tester1"}
+        # send friend request
+        conn = post(conn, Routes.user_friend_path(conn, :create, friender), %{friendee: friendee})
+        assert json_response(conn, 200) == %{"success" => false, "error" => "User does not exist"}
+    end
+end
