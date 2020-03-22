cb06899b3bc7bec9ed464c73914acd50ffa78fd4
Ian Ryan <xgix57@gmail.com>
2020-03-21 22:34:54 -0400

    Revert "Jack password"

--- lib/streamshore_web/controllers/user_controller.ex
+++ lib/streamshore_web/controllers/user_controller.ex
@@ -1,14 +1,8 @@
 defmodule StreamshoreWeb.UserController do
-  import Ecto.Query, only: [from: 2]
   use StreamshoreWeb, :controller
   alias Streamshore.Repo
   alias Streamshore.User
-<<<<<<< HEAD
-  alias Streamshore.Friends
-  
-=======
 
->>>>>>> master
   def index(conn, _params) do
     users = Repo.all(User)
     render(conn, "index.html", users: users)
@@ -25,26 +19,11 @@ defmodule StreamshoreWeb.UserController do
 
   def create(conn, params) do
     username = params["username"]
-    password = params["password"]
-    valid_pass = User.valid_password(password)
-    if !valid_pass do
-      json(conn, %{success: false, errors: "password: password is invalid"})
-    else 
-      successful =
-      %Streamshore.User{}
-      |> User.changeset(params)
-      |> Repo.insert()
+    successful =
+    %Streamshore.User{}
+    |> User.changeset(params)
+    |> Repo.insert()
 
-<<<<<<< HEAD
-      case successful do
-        {:ok, schema}->
-          json(conn, %{success: true, username: username})
-
-        {:error, changeset}->
-          errors = User.convert_changeset_errors(changeset)
-          json(conn, %{success: false, errors: errors})
-      end
-=======
     case successful do
       {:ok, _schema}->
         json(conn, %{success: true, username: username})
@@ -54,7 +33,6 @@ defmodule StreamshoreWeb.UserController do
         key = Enum.at(Map.keys(errors), 0)
         err = Atom.to_string(key) <> " " <> Enum.at(errors[key], 0)
         json(conn, %{success: false, error_msg: String.capitalize(err)})
->>>>>>> master
     end
   end
 
@@ -62,36 +40,6 @@ defmodule StreamshoreWeb.UserController do
     # TODO: show user info
   end
 
-<<<<<<< HEAD
-  def update(conn, params) do
-    username = params["username"]
-    user = User |> Repo.get_by(username: username)
-    password = params["password"]
-    valid_pass = User.valid_password(password)
-    if !valid_pass do
-      json(conn, %{success: false, error: "password: password is invalid"})
-    else
-      password = Bcrypt.hash_pwd_salt(password)
-      changeset = User.changeset(user, %{password: password})
-      successful = Repo.update(changeset)
-      case successful do
-        {:ok, schema}->
-          json(conn, %{success: true})
-
-        {:error, changeset}->
-          json(conn, %{success: false})
-      end
-    end
-  end
-
-  def delete(conn, params) do
-    username = params["username"]
-    user = User |> Repo.get_by(username: username)
-    successful = Repo.delete(user)
-    case successful do
-      {:ok, schema}->
-        json(conn, %{success: true})
-=======
   def update(_conn, _params) do
     # TODO: profile edit action
   end
@@ -99,10 +47,5 @@ defmodule StreamshoreWeb.UserController do
   def delete(_conn, _params) do
     # TODO: delete user
   end
->>>>>>> master
 
-      {:error, changeset}->
-        json(conn, %{success: false})
-    end
-  end
 end
