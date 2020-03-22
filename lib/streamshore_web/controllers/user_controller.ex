66894f3096e1d8af618d17755c23d3ee73f57bf4
Next Infinity <xgix57@gmail.com>
2020-03-21 21:01:02 -0400

    Util

--- lib/streamshore_web/controllers/user_controller.ex
+++ lib/streamshore_web/controllers/user_controller.ex
@@ -2,19 +2,11 @@ defmodule StreamshoreWeb.UserController do
   use StreamshoreWeb, :controller
   alias Streamshore.Repo
   alias Streamshore.User
+  alias Streamshore.Util
 
   def index(conn, _params) do
     users = Repo.all(User)
     render(conn, "index.html", users: users)
-  end
-
-  def convert_changeset_errors(changeset) do
-    out =  Ecto.Changeset.traverse_errors(changeset, fn {msg, opts} ->
-      Enum.reduce(opts, msg, fn {key, value}, acc ->
-        String.replace(acc, "%{#{key}}", to_string(value))
-      end)
-    end)
-    out
   end
 
   def create(conn, params) do
@@ -29,7 +21,7 @@ defmodule StreamshoreWeb.UserController do
         json(conn, %{success: true, username: username})
 
       {:error, changeset}->
-        errors = convert_changeset_errors(changeset)
+        errors = Util.convert_changeset_errors(changeset)
         key = Enum.at(Map.keys(errors), 0)
         err = Atom.to_string(key) <> " " <> Enum.at(errors[key], 0)
         json(conn, %{success: false, error_msg: String.capitalize(err)})
