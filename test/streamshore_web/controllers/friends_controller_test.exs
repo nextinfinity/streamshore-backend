74f6bfefe06ae7ff2e24460b45b9f008fdc60fb7
jsovich <jsovich@purdue.edu>
2020-03-23 15:37:51 -0400

    List of friends test

--- test/streamshore_web/controllers/friends_controller_test.exs
+++ test/streamshore_web/controllers/friends_controller_test.exs
@@ -29,7 +29,22 @@ defmodule FriendsControllerTest do
     end
 
     test "Getting a list of friends", %{conn: conn} do
-        
+        friender = "Tester1"
+        friendee = "Tester2"
+        # insert users into database
+        conn = post(conn, Routes.user_path(conn, :create), %{email: "Test@Test.com", username: friender, password: "$Test123"})
+        assert json_response(conn, 200) == %{"success" => true, "username" => "Tester1"}
+        conn = post(conn, Routes.user_path(conn, :create), %{email: "Test@Tester.com", username: friendee, password: "$Test123"})
+        assert json_response(conn, 200) == %{"success" => true, "username" => "Tester2"}
+        # send friend request
+        conn = post(conn, Routes.user_friend_path(conn, :create, friender), %{friendee: friendee})
+        assert json_response(conn, 200) == %{"success" => true}
+        # accept friend request
+        conn = put(conn, Routes.user_friend_path(conn, :update, friendee, friender), %{accepted: "1"})
+        assert json_response(conn, 200) == %{"success" => true}
+        # get list of friends
+        conn = get(conn, Routes.user_friend_path(conn, :index, friender))
+        assert json_response(conn, 200) == %{"friends" => [%{"friendee" => "Tester2", "nickname" => nil}], "requests" => []}
     end
 
     test "Users cannot see other users friends", %{conn: conn} do
