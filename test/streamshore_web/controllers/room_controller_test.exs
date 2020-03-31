3d915c59933c9b64a5f88ba414f66c6e9892e9d7
jsovich <jsovich@purdue.edu>
2020-03-30 23:33:23 -0400

    Testing for Rooms

--- test/streamshore_web/controllers/room_controller_test.exs
+++ test/streamshore_web/controllers/room_controller_test.exs
@@ -19,4 +19,22 @@ defmodule RoomControllerTest do
     |> leave
   end
 
+  test "creating a room", %{conn: conn} do
+    conn = post(conn, Routes.room_path(conn, :create), %{name: "Name", description: "", privacy: 0, owner: "user"})
+    assert json_response(conn, 200) == %{"route" => "name", "success" => true}
+  end
+
+  test "getting a room from database", %{conn: conn} do
+    conn = post(conn, Routes.room_path(conn, :create), %{name: "Name", description: "", privacy: 0, owner: "user"})
+    assert json_response(conn, 200) == %{"route" => "name", "success" => true}
+    list = conn
+           |> get(Routes.room_path(conn, :index))
+           |> json_response(200)
+    assert Enum.at(list, 0)["name"] == "Name"
+    assert Enum.at(list, 0)["owner"] == "user"
+    assert Enum.at(list, 0)["route"] == "name"
+    assert Enum.at(list, 0)["privacy"] == 0
+    assert Enum.at(list, 0)["thumbnail"] == nil
+    assert Enum.at(list, 0)["users"] == 0
+  end
 end
