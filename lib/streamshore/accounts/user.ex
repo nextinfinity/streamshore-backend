c13f9964f1ea0e190aa54154328901146cc2da51
Next Infinity <xgix57@gmail.com>
2020-04-14 12:29:53 -0400

    - -> _

--- lib/streamshore/accounts/user.ex
+++ lib/streamshore/accounts/user.ex
@@ -8,8 +8,8 @@ defmodule Streamshore.User do
         field(:password, :string)
         field(:room, :string)
         field(:admin, :integer, default: 0)
-        field(:verify-token, :string, default: nil)
-        field(:reset-token, :string, default: nil)
+        field(:verify_token, :string, default: nil)
+        field(:reset_token, :string, default: nil)
 
         timestamps()
         # field(:token, :joken)
