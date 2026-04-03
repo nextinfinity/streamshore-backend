52a885be6afd23c6d8b2fcb0497d6f3a0ce4aa5c
Ian Ryan <xgix57@gmail.com>
2026-04-02 20:40:13 -0700

    Remote unused Guardian functions

--- lib/streamshore/guardian.ex
+++ lib/streamshore/guardian.ex
@@ -4,18 +4,6 @@ defmodule Streamshore.Guardian do
   alias Streamshore.Accounts
   alias Streamshore.RoomPermissions
 
-  def subject_for_token(user, _claims) do
-    sub = to_string(user)
-    {:ok, sub}
-  end
-
-  def resource_from_claims(claims) do
-    user = claims["sub"]
-    anon = claims["anon"]
-    resource = %{user: user, anon: anon}
-    {:ok, resource}
-  end
-
   def token_from_conn(conn) do
     case Enum.find(conn.req_headers, fn {key, _value} ->
            String.downcase(key) == "authorization"
